a21f43354b861f280768bbaac083fae6e48d0c73
Lucas Unruh <lucasdunruh@gmail.com>
2020-06-15 20:18:32 -0400

    Heroku setup
    Adding elements for Heroku deployment

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const PORT = 4000;
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-app.use(cors());
-app.use(bodyParser.json());
+let port = process.env.PORT
+if (port == null || port === '') {
+  port = 8000
+}
+app.listen(port)
 
-app.listen(PORT, function() {
-    console.log("Server is running on Port: " + PORT);
-});
+app.use(cors())
+app.use(bodyParser.json())
+
+app.listen(port, function () {
+  console.log('Server is running on Port: ' + port)
+})
