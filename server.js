31568a17e19cc3a94d543ec4b759f1f557da2fff
Lucas Unruh <lucasdunruh@gmail.com>
2020-06-16 00:03:13 -0400

    Fixing CORS issue
    Enabling CORS in the proper location

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ if (port == null || port === '') {
   port = 8000
 }
 
+app.use(cors())
+app.use(bodyParser.json())
+
 app.get('/', function (req, res) {
   res.send('Get root test')
 })
-
-app.use(cors())
-app.use(bodyParser.json())
 
 app.listen(port, function () {
   console.log('Server is running on Port: ' + port)
