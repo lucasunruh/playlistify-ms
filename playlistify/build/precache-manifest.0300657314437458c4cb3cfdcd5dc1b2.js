43e607f0734e7958bc3475f8bafef04810fae108
Lucas Unruh <lucasdunruh@gmail.com>
2020-10-27 21:59:15 -0400

    Re-tracking build folder
    Mistakenly un-tracked

---
+++ playlistify/build/precache-manifest.0300657314437458c4cb3cfdcd5dc1b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f057c36ea0b00d6a00308209d04906c1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a9834e197cde99cda5fc",
+    "url": "./static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "b6fc1bf74307193865c1",
+    "url": "./static/css/main.9cf9578c.chunk.css"
+  },
+  {
+    "revision": "a9834e197cde99cda5fc",
+    "url": "./static/js/2.d9251aed.chunk.js"
+  },
+  {
+    "revision": "451ffe720aa8add9f554b8ed54913322",
+    "url": "./static/js/2.d9251aed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6fc1bf74307193865c1",
+    "url": "./static/js/main.47543850.chunk.js"
+  },
+  {
+    "revision": "957e772a3bf8d045ab7a",
+    "url": "./static/js/runtime-main.f06a563e.js"
+  }
+]);
