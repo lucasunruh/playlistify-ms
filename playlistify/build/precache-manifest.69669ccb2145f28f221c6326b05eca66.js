99978f29d899303cad9b456e396d799eb25998a7
Lucas Unruh <lucasdunruh@gmail.com>
2020-11-08 12:43:43 -0500

    Updated build
    Updated build for deployment

---
+++ playlistify/build/precache-manifest.69669ccb2145f28f221c6326b05eca66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e23a89328eeda9f8db6b4e2a42225e7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2d55b2c23d6ef5a0fbe0",
+    "url": "./static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "fda1a1b6cf5bd52ebbad",
+    "url": "./static/css/main.9cf9578c.chunk.css"
+  },
+  {
+    "revision": "2d55b2c23d6ef5a0fbe0",
+    "url": "./static/js/2.e930cf87.chunk.js"
+  },
+  {
+    "revision": "451ffe720aa8add9f554b8ed54913322",
+    "url": "./static/js/2.e930cf87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fda1a1b6cf5bd52ebbad",
+    "url": "./static/js/main.58c169f5.chunk.js"
+  },
+  {
+    "revision": "957e772a3bf8d045ab7a",
+    "url": "./static/js/runtime-main.f06a563e.js"
+  }
+]);
